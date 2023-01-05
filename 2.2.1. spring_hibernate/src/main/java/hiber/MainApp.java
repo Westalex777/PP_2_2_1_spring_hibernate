72e0a55cab8b2612de930565cb3ba8f761ae9a8d
Westalex777 <westalex777@yandex.ru>
2023-01-06 03:01:34 +0600

    comnit

--- 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
@@ -1,6 +1,7 @@
 package hiber;
 
 import hiber.config.AppConfig;
+import hiber.model.Car;
 import hiber.model.User;
 import hiber.service.UserService;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -15,10 +16,29 @@ public class MainApp {
 
       UserService userService = context.getBean(UserService.class);
 
-      userService.add(new User("User1", "Lastname1", "user1@mail.ru"));
-      userService.add(new User("User2", "Lastname2", "user2@mail.ru"));
-      userService.add(new User("User3", "Lastname3", "user3@mail.ru"));
-      userService.add(new User("User4", "Lastname4", "user4@mail.ru"));
+      Car car1 = new Car("Mercedes", "E600");
+      Car car2 = new Car("BMW", "X5");
+      Car car3 = new Car("Audi", "A8");
+      Car car4 = new Car("BMW", "535");
+
+      User user1 = new User("User1", "Lastname1", "user1@mail.ru");
+      User user2 = new User("User2", "Lastname2", "user2@mail.ru");
+      User user3 = new User("User3", "Lastname3", "user3@mail.ru");
+      User user4 = new User("User4", "Lastname4", "user4@mail.ru");
+
+      user1.setCar(car1);
+      car1.setUser(user1);
+      user2.setCar(car2);
+      car2.setUser(user2);
+      user3.setCar(car3);
+      car3.setUser(user3);
+      user4.setCar(car4);
+      car4.setUser(user4);
+
+      userService.add(user1);
+      userService.add(user2);
+      userService.add(user3);
+      userService.add(user4);
 
       List<User> users = userService.listUsers();
       for (User user : users) {
@@ -26,8 +46,12 @@ public class MainApp {
          System.out.println("First Name = "+user.getFirstName());
          System.out.println("Last Name = "+user.getLastName());
          System.out.println("Email = "+user.getEmail());
+         System.out.println(user.getCar());
          System.out.println();
       }
+
+      System.out.println("--------------------------------");
+      System.out.println("Вывод User по модели и серии Car : " + userService.getCarFromBD("BMW", "X5"));
 
       context.close();
    }
